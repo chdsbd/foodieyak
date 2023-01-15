31c9e09d4f4ce9e0ce8ddca62daa9e3911371923
Christopher Dignam <chris@dignam.xyz>
2023-01-15 13:02:30 -0500

    MenuItemDetailView

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -359,36 +359,40 @@ function PlacesDetailView() {
         <TabPanels>
           <TabPanel>
             {menu.map((m) => (
-              <HStack>
-                <LocationImage />
-                <Text fontSize={"xl"}>{m.name}</Text>
-                <Spacer />
-                <ButtonGroup>
-                  <Button
-                    colorScheme={m.selectedRating > 0 ? "green" : undefined}
-                  >
-                    ↑{m.rating.positive}
-                  </Button>
-                  <Button
-                    colorScheme={m.selectedRating < 0 ? "red" : undefined}
-                  >
-                    ↓{m.rating.negative}
-                  </Button>
-                </ButtonGroup>
-              </HStack>
+              <Link to="/place/tenoch/menu/potato">
+                <HStack>
+                  <LocationImage />
+                  <Text fontSize={"xl"}>{m.name}</Text>
+                  <Spacer />
+                  <ButtonGroup>
+                    <Button
+                      colorScheme={m.selectedRating > 0 ? "green" : undefined}
+                    >
+                      ↑{m.rating.positive}
+                    </Button>
+                    <Button
+                      colorScheme={m.selectedRating < 0 ? "red" : undefined}
+                    >
+                      ↓{m.rating.negative}
+                    </Button>
+                  </ButtonGroup>
+                </HStack>
+              </Link>
             ))}
           </TabPanel>
           <TabPanel>
             {checkIns.map((c) => (
-              <HStack>
-                <LocationImage />
-                <VStack align={"start"}>
-                  <div>{c.name}</div>
-                  <div>{c.creationTs}</div>
-                </VStack>
-                <Spacer />
-                <Rating ratings={c.ratings} />
-              </HStack>
+              <Link to="/place/tenoch/menu/potato">
+                <HStack>
+                  <LocationImage />
+                  <VStack align={"start"}>
+                    <div>{c.name}</div>
+                    <div>{c.creationTs}</div>
+                  </VStack>
+                  <Spacer />
+                  <Rating ratings={c.ratings} />
+                </HStack>
+              </Link>
             ))}
           </TabPanel>
         </TabPanels>
@@ -551,6 +555,83 @@ function NoMatch() {
   );
 }
 
+function MenuItemDetailView() {
+  const menuItem = {
+    name: "Potato",
+    positive: 4,
+    negative: 2,
+    ratings: [
+      { name: "Joe Shmoe", date: "2022-11-20", rating: 1 },
+      { name: "Tommy Enterprise", date: "2022-11-20", rating: -1 },
+    ],
+  };
+  return (
+    <VStack spacing={4}>
+      <HStack w="full">
+        <HomeButton />
+      </HStack>
+      <Breadcrumb alignSelf={"start"}>
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/">
+            Home
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/place/tenoch">
+            Tenoch
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink as={Link} to="/place/tenoch/menu/potato">
+            {menuItem.name}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <HStack w="100%">
+        <LocationImage />
+        <div>
+          <div>Tenoch</div>
+          <div>Medford, MA</div>
+        </div>
+      </HStack>
+
+      <HStack width="100%">
+        <Heading alignSelf={"start"} fontSize="2xl">
+          {menuItem.name}
+        </Heading>
+        <Spacer />
+        <ButtonGroup>
+          <Button>↑{menuItem.positive}</Button>
+          <Button>↓{menuItem.negative}</Button>
+        </ButtonGroup>
+      </HStack>
+
+      {menuItem.ratings.map((m) => (
+        <HStack width="100%">
+          <HStack>
+            <LocationImage />
+            <VStack align="start">
+              <div>{m.name}</div>
+              <div>{m.date}</div>
+            </VStack>
+          </HStack>
+          <Spacer />
+          <ButtonGroup>
+            <Button>
+              <ThumbsUp fill={m.rating > 0 ? "lightgreen" : "transparent"} />
+            </Button>
+            <Button>
+              <ThumbsDown fill={m.rating < 0 ? "orange" : "transparent"} />
+            </Button>
+          </ButtonGroup>
+        </HStack>
+      ))}
+    </VStack>
+  );
+}
+
 function App() {
   return (
     <Container padding={2}>
@@ -558,6 +639,9 @@ function App() {
         <Switch>
           <Route path="/place/:place/check-in/:checkin">
             <CheckInDetailView />
+          </Route>
+          <Route path="/place/:place/menu/:menuitem">
+            <MenuItemDetailView />
           </Route>
           <Route path="/place/:id/check-in">
             <CheckInCreateView />
