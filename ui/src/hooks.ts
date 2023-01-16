94327206e5c843c4d08d192e3db9d4c99374b8f3
Steve Dignam <steve@dignam.xyz>
2023-01-15 21:06:22 -0500

    use useeffect for onAuthStateChanged

--- ui/src/hooks.ts
+++ ui/src/hooks.ts
@@ -1,17 +1,19 @@
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
-import { useState } from "react";
+import React, { useState } from "react";
 
 export function useIsAuthed(): "authed" | "unauthed" | "loading" {
   const auth = getAuth();
   const [state, setState] = useState<"authed" | "unauthed" | "loading">(
     "loading"
   );
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setState("authed");
-    } else {
-      setState("unauthed");
-    }
-  });
+  React.useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setState("authed");
+      } else {
+        setState("unauthed");
+      }
+    });
+  }, []);
   return state;
 }
