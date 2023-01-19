37a4c9893b6287cb5007ddd71d840ca141e676bf
Steve Dignam <steve@dignam.xyz>
2023-01-18 19:38:26 -0500

    add: sentry

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  // TODO: move to env var
+  dsn: "https://a2e76d2f6b1c435897d9018b9f332cf5@o64108.ingest.sentry.io/4504528386916352",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
+// todo: rework how we initialize this
 import { db } from "./db";
 
 console.log(db.app.name);
