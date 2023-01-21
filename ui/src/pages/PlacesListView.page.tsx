2789b7900d77e4c4487110112e7197e573ea510f
Christopher Dignam <chris@dignam.xyz>
2023-01-21 14:23:40 -0500

    search

--- ui/src/pages/PlacesListView.page.tsx
+++ ui/src/pages/PlacesListView.page.tsx
@@ -1,4 +1,5 @@
 import { Button, HStack, Input, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { EmptyStateText } from "../components/EmptyStateText";
 import { LocationImage } from "../components/LocationImage";
@@ -8,6 +9,7 @@ import { usePlaces, useUser } from "../hooks";
 export function PlacesListView() {
   const user = useUser();
   const places = usePlaces(user.data?.uid);
+  const [search, setSearch] = useState("");
 
   return (
     <Page
@@ -20,18 +22,35 @@ export function PlacesListView() {
       {places !== "loading" && (
         <>
           {places.length === 0 && <EmptyStateText>No Places</EmptyStateText>}
-          {places.length > 0 && <Input placeholder="Search" type="search" />}
+          {places.length > 0 && (
+            <Input
+              placeholder="Search"
+              type="search"
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+            />
+          )}
+
+          {places.filter((x) =>
+            x.name.toLowerCase().includes(search.toLowerCase().trim())
+          ).length === 0 && <EmptyStateText>No matching places</EmptyStateText>}
 
           <VStack w="full">
-            {places.map((place) => (
-              <HStack as={Link} to={`/place/${place.id}`} w="full">
-                <LocationImage />
-                <div>
-                  <div>{place.name}</div>
-                  <div>{place.location}</div>
-                </div>
-              </HStack>
-            ))}
+            {places
+              .filter((x) =>
+                x.name.toLowerCase().includes(search.toLowerCase().trim())
+              )
+              .map((place) => (
+                <HStack as={Link} to={`/place/${place.id}`} w="full">
+                  <LocationImage />
+                  <div>
+                    <div>{place.name}</div>
+                    <div>{place.location}</div>
+                  </div>
+                </HStack>
+              ))}
           </VStack>
         </>
       )}
