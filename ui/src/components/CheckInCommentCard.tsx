d1e305b3db317416d7197542036a6cdf8d9e25ec
Christopher Dignam <chris@dignam.xyz>
2023-01-22 19:24:28 -0500

    wip

--- ui/src/components/CheckInCommentCard.tsx
+++ ui/src/components/CheckInCommentCard.tsx
@@ -12,17 +12,13 @@ import { formatHumanDateTime } from "../date"
 import { UserIdToName } from "../pages/FriendsListView.page"
 
 function Rating(props: { ratings: PlaceCheckIn["ratings"] }) {
-  const total = props.ratings.length
   const positive = props.ratings.filter((x) => x.rating > 0).length
   const negative = props.ratings.filter((x) => x.rating < 0).length
   return (
-    <HStack alignItems={"start"}>
-      <div>{total} items reviewed</div>
-      <div>
-        {positive > 0 && <>{positive}↑ </>}
-        {negative > 0 && <>{negative}↓</>}
-      </div>
-    </HStack>
+    <div>
+      {positive > 0 && <>{positive}↑ </>}
+      {negative > 0 && <>{negative}↓</>}
+    </div>
   )
 }
 
