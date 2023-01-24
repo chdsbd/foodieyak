8b2d2c4652b69d1d30ee0ddfddc2ddae3ada1579
Christopher Dignam <chris@dignam.xyz>
2023-01-24 08:50:23 -0500

    add staging config

--- ui/src/db.ts
+++ ui/src/db.ts
@@ -4,8 +4,7 @@ import { enableIndexedDbPersistence, getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+const prodFirebaseConfig = {
   apiKey: "AIzaSyCjtXB9vYR7qNm3EW6pLomMEVWVP4OhvtI",
   authDomain: "foodieyak-ef36d.firebaseapp.com",
   projectId: "foodieyak-ef36d",
@@ -14,8 +13,33 @@ const firebaseConfig = {
   appId: "1:886799226153:web:cd7693aec808276c741338",
 }
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const stageFirebaseConfig = {
+  apiKey: "AIzaSyANGQOnH9oQPFSfuYHKzRchXn6IedoXQlU",
+  authDomain: "foodieyak-staging.firebaseapp.com",
+  projectId: "foodieyak-staging",
+  storageBucket: "foodieyak-staging.appspot.com",
+  messagingSenderId: "227275725965",
+  appId: "1:227275725965:web:7d4a3c7f08bf07c3c916c4",
+}
+
+function getEnvironment(): "staging" | "production" {
+  if (
+    window.location.hostname.includes("localhost") ||
+    window.location.hostname.includes("staging")
+  ) {
+    return "staging"
+  }
+  return "production"
+}
+
+const environment = getEnvironment()
+
+// eslint-disable-next-line no-console
+console.info(`environment: ${environment}`)
+
+const app = initializeApp(
+  getEnvironment() === "production" ? prodFirebaseConfig : stageFirebaseConfig,
+)
 
 export const db = getFirestore(app)
 
