4cc37bc743a54ea442603f8a75870e49447537e8
Christopher Dignam <chris@dignam.xyz>
2023-03-25 15:28:32 -0400

    refactor activity page (#84)
    ### Before
    <img width="1624" alt="Screenshot 2023-03-25 at 2 52 56 PM" src="https://user-images.githubusercontent.com/1929960/227736031-5c9d4120-fe83-4928-a4b1-e8c680769cc3.png">
    
    ### After (v1)
    <img width="1624" alt="Screenshot 2023-03-25 at 2 52 46 PM" src="https://user-images.githubusercontent.com/1929960/227736030-4e8aef78-0dd0-4b12-b0f4-cd77eeb59c40.png">
    
    ### After (v2)
    <img width="1624" alt="Screenshot 2023-03-25 at 3 26 41 PM" src="https://user-images.githubusercontent.com/1929960/227737436-bfa63928-4bd9-4803-91a3-d86fd69b20da.png">

--- ui/src/pages/ActivityView.page.tsx
+++ ui/src/pages/ActivityView.page.tsx
@@ -1,11 +1,4 @@
 import {
-  AddIcon,
-  CheckIcon,
-  CloseIcon,
-  RepeatIcon,
-  WarningIcon,
-} from "@chakra-ui/icons"
-import {
   Box,
   Heading,
   HStack,
@@ -14,6 +7,8 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react"
+import { format, formatISO } from "date-fns"
+import { chain } from "lodash-es"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
@@ -21,7 +16,7 @@ import { placeById } from "../api"
 import { Activity } from "../api-schemas"
 import { assertNever } from "../assertNever"
 import { Page } from "../components/Page"
-import { formatHumanDateTime } from "../date"
+import { formatHumanDate } from "../date"
 import { useActivities, useUser } from "../hooks"
 import {
   pathCheckinDetail,
@@ -29,67 +24,6 @@ import {
   pathPlaceDetail,
 } from "../paths"
 import { UserIdToName } from "./FriendsListView.page"
-
-function Profile({ children }: { children: React.ReactNode }) {
-  const size = 32
-  return (
-    <Box
-      style={{
-        height: size,
-        minHeight: size,
-        minWidth: size,
-        width: size,
-        borderWidth: "1px",
-        border: "1px solid gray",
-        color: "inherit",
-        borderRadius: "100%",
-        justifyContent: "center",
-        display: "flex",
-        fontWeight: "bold",
-        alignItems: "center",
-      }}
-    >
-      {children}
-    </Box>
-  )
-}
-
-function ActionIcon({
-  type,
-  document,
-}: {
-  type: "create" | "delete" | "update" | "merge"
-  document: Activity["document"]
-}) {
-  // TODO: better icons
-  if (type === "create") {
-    if (document === "checkin") {
-      return (
-        <Profile>
-          <CheckIcon color="green.400" />
-        </Profile>
-      )
-    }
-    return (
-      <Profile>
-        <AddIcon color="green.400" />
-      </Profile>
-    )
-  }
-  return (
-    <Profile>
-      {type === "delete" ? (
-        <CloseIcon boxSize={3} color="red.400" />
-      ) : type === "update" ? (
-        <RepeatIcon color="blue.400" />
-      ) : type === "merge" ? (
-        <WarningIcon color="yellow.400" />
-      ) : (
-        assertNever(type)
-      )}
-    </Profile>
-  )
-}
 
 type ActivityType = "create" | "delete" | "update" | "merge"
 
@@ -111,7 +45,6 @@ function updateTypeToStr(type: ActivityType): string {
 
 const linkStyled = {
   fontWeight: "500",
-  textDecoration: "underline",
 }
 
 function updateDescription({
@@ -124,6 +57,20 @@ function updateDescription({
   activity: Activity
 }) {
   if (activity.document === "checkin") {
+    if (update === "create") {
+      return (
+        <>
+          created a{" "}
+          <Text
+            as={Link}
+            {...linkStyled}
+            to={pathCheckinDetail({ placeId, checkInId: activity.checkinId })}
+          >
+            checkin
+          </Text>{" "}
+        </>
+      )
+    }
     return (
       <>
         {updateTypeToStr(update)} a{" "}
@@ -195,84 +142,59 @@ function usePlaceName({ placeId }: { placeId: string }): string {
   return name
 }
 
-function Action({
-  update,
-  placeId,
+function Activity({
+  type,
   activity,
 }: {
-  update: ActivityType
-  placeId: string
+  type: ActivityType
   activity: Activity
 }) {
   const description = updateDescription({
-    update,
-    placeId,
+    update: type,
+    placeId: activity.placeId,
     activity,
   })
-  const placeName = usePlaceName({ placeId })
   return (
-    <>
-      {description}{" "}
-      <Text as={Link} {...linkStyled} to={pathPlaceDetail({ placeId })}>
-        {placeName}
-      </Text>
-    </>
-  )
-}
-
-function Description({
-  update,
-  placeId,
-  activity,
-}: {
-  placeId: string
-  update: ActivityType
-  activity: Activity
-}) {
-  return (
-    <HStack spacing={1}>
+    <HStack spacing={1} width="100%" justifyContent={"space-between"}>
       <div>
         <span style={{ fontWeight: "bold" }}>
           <UserIdToName userId={activity.createdById} />
         </span>{" "}
-        <Action update={update} placeId={placeId} activity={activity} />
+        {description}{" "}
       </div>
+      <Box>{format(activity.createdAt.toDate(), "h:mmaaa")}</Box>
     </HStack>
   )
 }
 
-function ActivityTimestamp({
-  timestamp,
-}: {
-  timestamp: Activity["createdAt"]
-}) {
-  return <div>{formatHumanDateTime(timestamp)}</div>
+function convertActivities(orderedActivities: Activity[]): {
+  date: string
+  placesWithActivities: { placeId: string; activities: Activity[] }[]
+}[] {
+  const res = chain(orderedActivities)
+    .groupBy((x) =>
+      formatISO(x.createdAt.toDate(), {
+        representation: "date",
+      }),
+    )
+    .map((activities, date) => {
+      const placeActivities = chain(activities)
+        .groupBy((x) => x.placeId)
+        .map((activities, placeId) => ({ activities, placeId }))
+        .value()
+      return { placesWithActivities: placeActivities, date }
+    })
+    .value()
+  return res
 }
 
-function Activity({
-  document,
-  type,
-  timestamp,
-  activity,
-}: {
-  document: Activity["document"]
-  type: ActivityType
-  timestamp: Activity["createdAt"]
-  activity: Activity
-}) {
-  return (
-    <HStack spacing={3}>
-      <ActionIcon type={type} document={document} />
+function PlaceIdToName({ placeId }: { placeId: string }) {
+  const placeName = usePlaceName({ placeId })
 
-      <VStack align="start" spacing={0}>
-        <Description
-          update={type}
-          placeId={activity.placeId}
-          activity={activity}
-        />
-        <ActivityTimestamp timestamp={timestamp} />
-      </VStack>
-    </HStack>
+  return (
+    <Text as={Link} {...linkStyled} to={pathPlaceDetail({ placeId })}>
+      {placeName}
+    </Text>
   )
 }
 
@@ -281,6 +203,8 @@ export function ActivityView() {
   const [filter, setFilter] = useState<SelectOption>("checkins")
   const user = useUser()
   const activities = useActivities({ filter, userId: user.data?.uid ?? "" })
+  const activityDays = convertActivities(activities)
+
   return (
     <Page>
       <HStack w="100%" alignItems={"center"}>
@@ -304,18 +228,26 @@ export function ActivityView() {
           <option value="everything">Everything</option>
         </Select>
       </HStack>
-      <VStack spacing={4} align="start">
-        {activities.map((a) => {
-          return (
-            <Activity
-              key={a.id}
-              document={a.document}
-              type={a.type}
-              timestamp={a.createdAt}
-              activity={a}
-            />
-          )
-        })}
+      <VStack spacing={6} align="start" width="100%">
+        {activityDays.map((day) => (
+          <Box key={day.date} width={"100%"}>
+            <Box borderBottomWidth={"1px"} marginBottom={2}>
+              {formatHumanDate(new Date(day.date))}
+            </Box>
+            <VStack spacing={3} width="100%" alignItems={"start"}>
+              {day.placesWithActivities.map((place) => (
+                <Box key={place.placeId} w="100%">
+                  <Box>
+                    <PlaceIdToName placeId={place.placeId} />
+                  </Box>
+                  {place.activities.map((a) => {
+                    return <Activity key={a.id} type={a.type} activity={a} />
+                  })}
+                </Box>
+              ))}
+            </VStack>
+          </Box>
+        ))}
       </VStack>
     </Page>
   )
