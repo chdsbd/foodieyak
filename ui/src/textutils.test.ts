ceca5a839e2bad0cfba8fcd979c1ceb5a8125aa5
Christopher Dignam <chris@dignam.xyz>
2023-04-02 18:34:11 -0400

    run UI tests

--- ui/src/textutils.test.ts
+++ ui/src/textutils.test.ts
@@ -3,7 +3,7 @@ import { startCase } from "./textutils"
 test.each([
   ["über corn", "Über Corn"],
   ["Braised 1/2 chicken", "Braised 1/2 Chicken"],
-  ["all lower case words", "All Lower Case Words"],
+  ["all lower case words", "All ower Case Words"],
 ])("startCase", (input: string, expected: string) => {
   expect(startCase(input)).toEqual(expected)
 })
