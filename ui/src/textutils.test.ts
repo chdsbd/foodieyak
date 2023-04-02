0a5216deb934171cdad07366ef9e41ec60c83cf9
Christopher Dignam <chris@dignam.xyz>
2023-04-02 18:50:10 -0400

    Revert "wip"
    This reverts commit 4e4a432a13f5149858ce1457b12da621f33385b3.

--- ui/src/textutils.test.ts
+++ ui/src/textutils.test.ts
@@ -5,6 +5,5 @@ test.each([
   ["Braised 1/2 chicken", "Braised 1/2 Chicken"],
   ["all lower case words", "All ower Case Words"],
 ])("startCase", (input: string, expected: string) => {
-  const x: string = 123
   expect(startCase(input)).toEqual(expected)
 })
