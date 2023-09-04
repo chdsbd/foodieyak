19c11ebe8e0eff475777ec81f001dac820ebb192
Christopher Dignam <chris@dignam.xyz>
2023-09-04 11:16:34 -0400

    .

--- ui/src/types/google-maps.d.ts
+++ ui/src/types/google-maps.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 declare namespace google.maps.marker {
   class PinView extends PinElement {}
   class AdvancedMarkerView extends AdvancedMarkerElement {}
