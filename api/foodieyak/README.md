d60f892a66833e09440b8547ec77d8c37a2fcc3f
Christopher Dignam <chris@dignam.xyz>
2023-12-03 13:01:40 -0500

    .

--- api/foodieyak/README.md
+++ api/foodieyak/README.md
@@ -4,3 +4,14 @@ Improve initial page load by:
 
 - eliminating the need to wait for Firebase auth token refresh
 - enable server side rendering
+
+## Development
+
+```bash
+gcloud config set project foodieyak-staging
+gcloud auth application-default login
+```
+
+```bash
+s/dev
+```
--- api/foodieyak/db.py
+++ api/foodieyak/db.py
@@ -1,8 +1,7 @@
 import firebase_admin
 from firebase_admin import firestore_async
 
-from foodieyak import config
-
-firebase_admin.initialize_app(config.FIREBASE_GOOGLE_SERVICE_ACCOUNT_CREDENTIALS)
+# credentials pulled from resource service account.
+firebase_admin.initialize_app()
 
 db = firestore_async.client()
