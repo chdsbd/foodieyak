8ec1509aa19fc810ffa27c26db4226f5ca710dd7
Christopher Dignam <chris@dignam.xyz>
2023-12-03 11:54:41 -0500

    .

--- api/foodieyak/config.py
+++ api/foodieyak/config.py
@@ -5,6 +5,11 @@ from starlette.config import Config
 
 config = Config(".env")
 
-FIREBASE_GOOGLE_SERVICE_ACCOUNT_CREDENTIALS = credentials.Certificate(
-    json.loads(config("FIREBASE_GOOGLE_SERVICE_ACCOUNT_CERTIFICATE"))
+GOOGLE_SERVICE_ACCOUNT_CERTIFICATE_JSON = config(
+    "GOOGLE_SERVICE_ACCOUNT_CERTIFICATE_JSON", default=None
+)
+FIREBASE_GOOGLE_SERVICE_ACCOUNT_CREDENTIALS = (
+    credentials.Certificate(json.loads(GOOGLE_SERVICE_ACCOUNT_CERTIFICATE_JSON))
+    if GOOGLE_SERVICE_ACCOUNT_CERTIFICATE_JSON
+    else None
 )
